f928cc02aaadb9a9acd822b230965aa285508d03
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-09-20 15:17:42 +0300

    add input and function in github-search.component.ts and github-search.component.html respectively that search for a user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    GithubSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/github-search/github-search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+@Component({
+  selector: 'app-github-search',
+  templateUrl: './github-search.component.html',
+  styleUrls: ['./github-search.component.css']
+})
+export class GithubSearchComponent implements OnInit {
+  link = 'https://api.github.com/users/';
+  accessToken= '?access_token=136fefe167f49c63fdd64405284a6c76df3f5f8c';
+  users=[];
+  constructor(private http: Http) { }
+  performSearch(search: HTMLInputElement): void{
+    let fullLink = this.link + search.value +this.accessToken;
+    this.http.get(fullLink).subscribe((res:Response)=>{
+      this.users= res.json();
+      console.log(this.users);
+    })
+  }
+  ngOnInit() {
+  }
+
+}
