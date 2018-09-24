46cd65c3d31e2bad0e75c1bdbc6581a68ba0cb88
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-09-24 10:56:51 +0300

    Add function to search for fatahrez in landing-page.component.ts

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment'
+import { Http, Response} from '@angular/http'
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -10,6 +12,19 @@ export class LandingPageComponent implements OnInit {
   accessToken= environment.accessToken;
   searchArray=[]; 
   constructor() { }
+  userArray=[];
+  performSearch(search: HTMLInputElement): void{
+    let userLink = this.link + search.value + this.accessToken;
+    this.http.get(userLink).subscribe((res:Response)=>{
+      this.userArray= res.json();
+      console.log(this.userArray);
+    })
+    let fullLink = this.link + search.value +"/repos" +this.accessToken;
+    this.http.get(fullLink).subscribe((res:Response)=>{
+      this.searchArray= res.json();
+      console.log(this.searchArray);
+    })
+  }
 
   ngOnInit() {
   }
